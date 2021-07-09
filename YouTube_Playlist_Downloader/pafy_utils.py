86d8bc4b7947f233d57933fcde64aabc7bb48fa8
rakib <rakibrk1811@gmail.com>
2021-07-09 11:14:52 +0530

    Key Exposed Fixed

--- YouTube_Playlist_Downloader/pafy_utils.py
+++ YouTube_Playlist_Downloader/pafy_utils.py
@@ -1,7 +1,7 @@
 from googleapiclient.discovery import build
 import pafy
 
-api_key = "AIzaSyDA6Lamtku-IO8kSurxdOvqh-PdKNu9HOw"
+api_key = "YOUTUBE DATA API V3 KEY" # ADD YOUR API KEY
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 
